f34f5ce7b4b466414bf4c335d63d5b37865c38ce
risanpallithazha <risanpallithazha@gmail.com>
2026-03-16 13:03:09 +0530

    Update API URL to AWS backend with HTTPS

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE = import.meta.env.VITE_API_URL || 'https://nizan-makeovers-attendence.onrender.com';
-const API_BASE = import.meta.env.VITE_API_URL || '';
+const API_BASE = import.meta.env.VITE_API_URL || 'https://35.154.161.122.nip.io';
 
 const api = axios.create({
   baseURL: API_BASE,
